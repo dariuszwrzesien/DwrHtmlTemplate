39b54e75bc156d4b2a06d9ea0b106881484013f5
Dariusz Wrzesien <dariuszwrzesien@gmail.com>
2017-02-22 21:45:58 +0100

    update form

---
+++ user-profile-template/upload.php
@@ -0,0 +1,11 @@
+<?php
+
+var_dump($_POST);
+var_dump($_FILES);
+
+/**
+ * In order to save only part of picture take X,Y,W,H
+ * and using GD create new picture.
+ */
+
+die(__FILE__ . ':'. __LINE__);
